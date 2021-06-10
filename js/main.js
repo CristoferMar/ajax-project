7dd408bd1b3ef349283b08c3c424ef7cc063428a
CristoferMar <cristoferm@gmail.com>
2021-06-10 16:03:11 -0700

    Make these buttons “toggleable” to switch between color and API

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ var $navBar = document.querySelector('.nav-container');
 var $uniformColor = document.querySelectorAll('.green');
 var $body = document.querySelector('body');
 
+var $searchBar = document.querySelector('.search-form');
+var $searchImg = document.querySelector('.spy');
+
 console.log($toggleBtn);
 
 $navBar.addEventListener('click', function (event) {
@@ -17,7 +20,13 @@ $navBar.addEventListener('click', function (event) {
 
   if (event.target.matches('.large-btn')) {
     console.log(event.target.textContent);
-    toggleDB();
+    clearGallery();
+    if (data.currentDBmeals === true) {
+      data.currentDBmeals = false;
+    } else {
+      data.currentDBmeals = true;
+    }
+    toggleDB(data.currentDBmeals);
 
 
   }
@@ -30,31 +39,26 @@ function clearGallery() {
   }
 }
 
-function toggleDB() {
-  clearGallery();
+function toggleDB(currentDB) {
 
-  if (data.currentDBmeals === true) {
-    data.currentDBmeals = false;
-  } else {
-    data.currentDBmeals = true;
-  }
-
-  if (data.currentDBmeals === false) {
+  if (currentDB === false) {
     $body.classList.replace('meal-background', 'drink-background');
     for (var i = 0; i < $uniformColor.length; i++) {
-      $uniformColor[i].classList.replace('green', 'purple')
+      $uniformColor[i].classList.replace('green', 'purple');
+      $searchBar.children[i].classList.replace('meal-search', 'drink-search');
     }
     $toggleBtn.classList.replace('purple', 'green');
     $toggleBtn.textContent = 'Meals';
-
+    $searchImg.setAttribute('src', 'images/Spy_Glass_White.svg');
   } else {
     $body.classList.replace('drink-background', 'meal-background');
     for (i = 0; i < $uniformColor.length; i++) {
       $uniformColor[i].classList.replace('purple', 'green');
-      $toggleBtn.classList.replace('green', 'purple');
-      $toggleBtn.textContent = 'Drink';
+      $searchBar.children[i].classList.replace('drink-search', 'meal-search');
     }
-
+    $toggleBtn.classList.replace('green', 'purple');
+    $toggleBtn.textContent = 'Drink';
+    $searchImg.setAttribute('src', 'images/Spy_Glass.svg');
   }
 
 
